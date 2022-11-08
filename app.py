b42add108373d3a31e1885b20236e2c8a98decab
GuyAv46 <guy.avimor@gmail.com>
2022-11-08 14:32:45 +0200

    make app search redis for real

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, request, send_file
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+from redis import Redis
+import numpy as np
+from redis.commands.json.path import Path
+from redis.commands.search.field import VectorField, TagField, NumericField
+from redis.commands.search.query import Query
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+import os
+import time
+import json
 
 app = Flask(__name__)
+redis = Redis()
+img2vec = Img2Vec()
+vector_field_name = "vectors"
 
 @app.route('/')
 def index():
@@ -12,12 +26,23 @@ def search():
         return 'no image found', 400
 
     file = request.files['image']
-    # TODO
-    file.save('test.jpg')
+    image = Image.open(file)
+    query_vector = img2vec.get_vec(image)
 
-    return {
-        'products': [
-            {"score": 0.1, "id":4916118,"names":{"short":"Candy Mentchees","long":"Candy Mentchees"},"brand":"Mitzvah Kinder","isWeighable":False,"family":{"name":"Toys"},"images":["https://storage.googleapis.com/sp-public/product-images/global/4916118/4927867/large.jpg"]},
-            {"score": 0.2, "id":5082713,"names":{"short":"Gourmet Red Wine Sauce","long":"Gourmet Red Wine Sauce"},"brand":"Jasmine Gourmet","isWeighable":False,"family":{"name":"Red Wine Vinegar"},"images":["https://storage.googleapis.com/sp-public/product-images/global/5082713/2736019/large.jpg"]}
-        ]
-    }
+    res, total_time = getTopK(query_vector, 4)
+
+    print(total_time)
+
+    return res
+
+def getTopK(query_vector, k = 10, filter = '*'):
+    q = Query(f'({filter})=>[KNN {k} @{vector_field_name} $vec_param]=>{{$yield_distance_as: dist}}').sort_by(f'dist').paging(0, k)
+    start = time.time()
+    res = redis.ft().search(q, query_params={'vec_param': query_vector.tobytes()})
+    search_time = time.time() - start
+
+    products = [json.loads(doc.json) for doc in res.docs]
+    for prod, doc in zip(products, res.docs):
+        prod['distance'] = doc.dist
+
+    return {'products': products}, search_time
