922d207e49d66908129ffecc69a7f66eb43fe00c
GuyAv46 <guy.avimor@gmail.com>
2022-11-09 14:43:24 +0200

    improved returned object from redis

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 from flask import Flask, request, send_file
-import cv2
 
 from img2vec_pytorch import Img2Vec
 from PIL import Image
 from redis import Redis
 import numpy as np
 import torch
-import torchvision
 from torchvision.models.detection import retinanet_resnet50_fpn_v2, RetinaNet_ResNet50_FPN_V2_Weights
 import torchvision.transforms as transforms
 
-from redis.commands.json.path import Path
-from redis.commands.search.field import VectorField, TagField, NumericField
 from redis.commands.search.query import Query
-from redis.commands.search.indexDefinition import IndexDefinition, IndexType
-import os
 import time
-import json
 
 app = Flask(__name__)
 redis = Redis()
@@ -36,7 +29,7 @@ transform = transforms.Compose([
 
 # Get the model
 def get_detection_model(device):
-    # load the model 
+    # load the model
     weights = RetinaNet_ResNet50_FPN_V2_Weights.COCO_V1
     model = retinanet_resnet50_fpn_v2(weights=weights)
     # load the model onto the computation device
@@ -49,10 +42,10 @@ def get_boxes(image, model, device, detection_threshold):
     # transform the image to tensor
     image = transform(image).to(device)
     # add a batch dimension
-    image = image.unsqueeze(0) 
+    image = image.unsqueeze(0)
     # get the predictions on the image
     with torch.no_grad():
-        outputs = model(image) 
+        outputs = model(image)
     # get score for all the predicted objects
     pred_scores = outputs[0]['scores'].detach().cpu().numpy()
     # get all the predicted bounding boxes
@@ -66,7 +59,7 @@ def search_product(image, box_points):
     product = image.crop(box_points)
     query_vector = img2vec.get_vec(product)
 
-    res, _ = getTopK(query_vector, 1)
+    res, _ = getTopK(query_vector, 4)
 
     return res
 
@@ -82,24 +75,15 @@ def search():
     file = request.files['image']
     image = Image.open(file)
 
-
-
     boxes = get_boxes(image, g_model, g_device, threshold)
 
-    print (boxes)
-
-    res =  [(box.tolist(), search_product(image, box)) for box in boxes]
-    print(res)
-    return res
+    return [{"box": box.tolist(), "products": search_product(image, box)} for box in boxes]
 
 def getTopK(query_vector, k = 10, filter = '*'):
-    q = Query(f'({filter})=>[KNN {k} @{vector_field_name} $vec_param AS dist]').sort_by(f'dist').paging(0, k)
+    q = Query(f'({filter})=>[KNN {k} @{vector_field_name} $vec_param AS distance]').sort_by('distance').paging(0, k)\
+        .return_fields('id', 'brand', 'name', 'family', 'distance').return_field('$.images[0]', 'image')
     start = time.time()
     res = redis.ft().search(q, query_params={'vec_param': query_vector.tobytes()})
     search_time = time.time() - start
 
-    products = [json.loads(doc.json) for doc in res.docs]
-    for prod, doc in zip(products, res.docs):
-        prod['distance'] = doc.dist
-
-    return {'products': products}, search_time
+    return [doc.__dict__ for doc in res.docs], search_time
