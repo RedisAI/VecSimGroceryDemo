cbf7b00c17d1a5b139d50d9b67217fb2b6b7744c
GuyAv46 <guy.avimor@gmail.com>
2022-11-13 16:51:47 +0200

    moved files to directories and created docker files

---
+++ app/app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, send_file
+
+from redis import Redis
+from redis.commands.search.query import Query
+
+import torch
+from torchvision import transforms
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+from yolov5.utils.general import non_max_suppression
+from yolov5.models.experimental import attempt_load
+
+from pathlib import Path
+import time
+
+FILE = Path(__file__).resolve()
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+redis = Redis(host='redis')
+img2vec = Img2Vec()
+
+weights = 'best.pt'
+g_conf_threshold = 0.2
+g_overlap_threshold = 0.7
+g_max_det = 10
+g_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+g_model = attempt_load(weights, device=g_device)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+# Get the model
+def get_boxes(image, model, conf_threshold, overlap_threshold, max_det):
+    # Prepare image
+    img = transform(image).to(g_device)[None]
+    # Run model
+    predictions = model(img)
+    # Remove overlapping (overlap_threshold), low confidence (conf_threshold) and worst (max_det) squares.
+    detections = non_max_suppression(predictions, conf_thres=conf_threshold, max_det=max_det, iou_thres=overlap_threshold)
+    # Return list of coordinations of the boxes
+    return detections[0][:, :4].tolist()
+
+#box_points = [xmin,ymin, xmax,ymax]
+def search_product(image, box_points):
+    product = image.crop(box_points)
+    query_vector = img2vec.get_vec(product)
+
+    res, search_time = getTopK(query_vector, 4)
+    print('KNN search time\t', search_time)
+    return res
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    if 'image' not in request.files:
+        return 'no image found', 400
+
+    file = request.files['image']
+    image = Image.open(file)
+
+    start = time.time()
+    boxes = get_boxes(image, g_model, g_conf_threshold, g_overlap_threshold, g_max_det)
+    print('boxing time\t', time.time() - start)
+
+    return {
+        'results': [
+            {
+                'box': box,
+                'products': search_product(image, box)
+            } for box in boxes
+        ]
+    }
+
+def getTopK(query_vector, k = 10, filter = '*'):
+    q = Query(f'({filter})=>[KNN {k} @vectors $vec_param AS distance]').sort_by('distance').paging(0, k).dialect(2)\
+        .return_fields('id', 'brand', 'name', 'family', 'distance').return_field('$.images[0].url', 'image')
+    start = time.time()
+    res = redis.ft().search(q, query_params={'vec_param': query_vector.tobytes()})
+    search_time = time.time() - start
+
+    return [doc.__dict__ for doc in res.docs], search_time
---
+++ data/main2.py
@@ -0,0 +1,116 @@
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+from redis import Redis
+import numpy as np
+from redis.commands.json.path import Path
+from redis.commands.search.field import VectorField, TagField, NumericField
+from redis.commands.search.query import Query
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+import os
+import time
+import json
+
+host = "localhost"
+port = 6379
+prod_prefix = 'product:'
+
+def create_index_in_redis(dim, vector_field_name):
+    redis = Redis(host=host, port=port)
+    redis.flushall()
+    schema = (
+        VectorField(f'$.{vector_field_name}[*]', "FLAT", {"TYPE": "FLOAT32", "DIM": dim, "DISTANCE_METRIC": "COSINE"}, as_name=vector_field_name),
+        TagField('$.brand', as_name='brand'),
+        NumericField('$.id', as_name='id'),
+        TagField("$.names.short", as_name='name'),
+        TagField("$.names.long", as_name='long_name'),
+        TagField("$.family.name", as_name='family'),
+        )
+    redis.ft().create_index(schema, definition=IndexDefinition(prefix=[prod_prefix], index_type=IndexType.JSON))
+    redis.ft().config_set("default_dialect", 2)
+
+
+def load_data(client, img2vec, vector_field_name):
+    count = 0
+    total_time = 0
+
+    map = {}
+    for file in os.listdir('data'):
+        split = file.split('.')
+
+        id = split[0].split('-')[0]
+        ext = split[-1]
+
+        if (not id in map):
+            map[id] = {
+                'json': None,
+                'images': []
+            }
+
+        if (ext == 'json'):
+            map[id]['json'] = file
+        else:
+            map[id]['images'].append(file)
+
+    for id in map:
+        # Read in an image (rgb format
+
+        if (map[id]['json'] == None):
+            print(id)
+            continue
+
+
+        try:
+            imgs = [Image.open(f'data/{img}') for img in map[id]['images']]
+            start = time.time()
+            vectors = img2vec.get_vec(imgs)
+            total_time += time.time() - start
+            # try:
+            with open(f'data/{map[id]["json"]}', 'r') as jfile: json_doc = json.load(jfile)
+            # except FileNotFoundError:
+            #     json_doc = {"id": i, "names": {"short": "not Found", "long": "File Not Found"}}
+            json_doc[vector_field_name] = [vec.tolist() for vec in vectors]
+            client.json().set(f"{prod_prefix}{id}", Path.root_path(), json_doc)
+            count += 1
+        except Exception as e:
+            print(f"Runtime error {e} for {id}")
+        finally:
+            [img.close() for img in imgs]
+
+    info = client.ft().info()
+    print("Index size: ", info['num_docs'])
+    print("Number of Records: ", info['num_records'])
+    print(f"Avg inference time is: {total_time/count}")
+
+
+def main():
+    dim = 512
+    vector_field_name = "vectors"
+    client = Redis(host=host, port=port)
+    create_index_in_redis(dim, vector_field_name)
+
+    # Initialize Img2Vec
+    img2vec = Img2Vec()
+
+    # Load images data, convert it to vector embeddings and store it in Redis.
+    load_data(client, img2vec, vector_field_name)
+
+    # Run KNN for some image
+    for fname in [img for img in os.listdir() if img.split('.')[-1] == 'jpg']:
+        print(fname)
+        img = Image.open(fname)
+        query_vector = img2vec.get_vec(img)
+        start = time.time()
+        q = Query(f'*=>[KNN 10 @{vector_field_name} $vec_param]=>{{$yield_distance_as: dist}}').sort_by(f'dist')
+        res = client.ft().search(q, query_params={'vec_param': query_vector.tobytes()})
+        end = time.time()
+
+        print(f"search took {end-start} seconds")
+
+        docs = [doc.id for doc in res.docs]
+        print(docs)
+        dists = [float(doc.dist) if hasattr(doc, 'dist') else '-' for doc in res.docs]
+        print(dists)
+
+
+if __name__ == '__main__':
+    main()
